6d30e02586b47f384ea74577648966e7a0b946ab
NickGrit <1505448035@qq.com>
2020-05-05 19:43:23 +0800

    init

---
+++ src/main/java/com/example/practice/TestDebug.java
@@ -0,0 +1,16 @@
+package com.example.practice;
+
+public class TestDebug {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        aa();
+        System.out.println("hello world");
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+
+    }
+}
